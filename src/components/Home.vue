fa2d07f382928285975f9ff20eca313b6d960922
LyublyanNedkov <lyublyannedkov@gmail.com>
2022-03-11 21:47:47 +0200

    updeit

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,20 @@
 <template>
-    <div>
-        <div class="container">
-            <div class="row justify-content-center align-items-center">
-                <div class="col-sm-12 col-md-12 col-xxl-12">
-                    <img class="icon" src="../assets/Icon.jpg" alt="icon">
-                </div>
+    <div class="container container-fluid container-home">
+        <div class="row justify-content-center">
+            <div class="col col-sm-12 col-lg-12 col-xxl-12 text-center">
+                <img class="icon" src="../assets/Icon.jpg" alt="icon">
             </div>
-            <div class="row">
-                <div class="col-12">
-                    <p class="ln">LN Photography</p>
-                </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col col-sm-12 col-lg-12 col-xxl-12 text-center">
+                <p class="ln">LN Photography</p>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col col-sm-12 col-lg-12 col-xxl-12 text-center">
+                <router-link class="audi-text" to="Audi">Sponsored by AUDI</router-link>
+                <br>
+                <img class="audi-img" src="../assets/Audi.jpg" alt="audi">
             </div>
         </div>
     </div>
@@ -25,4 +30,3 @@
 
 </style>
 
-             
