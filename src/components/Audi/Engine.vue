361e3f088a6e75ca432dfa4ab14d3261a2b8ed80
LyublyanNedkov <lyublyannedkov@gmail.com>
2022-03-31 22:45:01 +0300

    LN

--- src/components/Audi/Engine.vue
+++ src/components/Audi/Engine.vue
@@ -14,6 +14,8 @@
                         <div class="card-header card-color">RS7</div>
                         <div class="card-body text-dark">
                             <img class="img-engine img-a" src="../../assets/RS7.jpg" alt="engine">
+                            <input type="number" class="form-control" placeholder="Quantity">
+                            <br>
                             <button class="btn btn-primary btn-cart" @click="addToCart(product)">ADD TO CART</button>
                         </div>
                     </div>
@@ -25,6 +27,8 @@
                         <div class="card-header">RS6-Avant</div>
                         <div class="card-body text-dark">
                             <img class="img-engine img-a" src="../../assets/RS6-Avant.jpg" alt="engine">
+                            <input type="number" class="form-control" placeholder="Quantity">
+                            <br>
                             <button class="btn btn-primary btn-cart" @click="addToCart(product)">ADD TO CART</button>
                         </div>
                     </div>
@@ -36,6 +40,8 @@
                         <div class="card-header">RS6</div>
                         <div class="card-body text-dark">
                             <img class="img-engine img-a" src="../../assets/RS6.jpg" alt="engine">
+                            <input type="number" class="form-control" placeholder="Quantity">
+                            <br>
                             <button class="btn btn-primary btn-cart" @click="addToCart(product)">ADD TO CART</button>
                         </div>
                     </div>
@@ -47,6 +53,8 @@
                         <div class="card-header">RS4-Avant</div>
                         <div class="card-body text-dark">
                             <img class="img-engine img-a" src="../../assets/RS4-Avant.jpg" alt="engine">
+                            <input type="number" class="form-control" placeholder="Quantity">
+                            <br>
                             <button class="btn btn-primary btn-cart" @click="addToCart(product)">ADD TO CART</button>
                         </div>
                     </div>
@@ -60,6 +68,8 @@
                         <div class="card-header">RS4</div>
                         <div class="card-body text-dark">
                             <img class="img-engine img-a" src="../../assets/RS4.jpg" alt="engine">
+                            <input type="number" class="form-control" placeholder="Quantity">
+                            <br>
                             <button class="btn btn-primary btn-cart" @click="addToCart(product)">ADD TO CART</button>
                         </div>
                     </div>
@@ -71,6 +81,8 @@
                         <div class="card-header">RS3</div>
                         <div class="card-body text-dark">
                             <img class="img-engine img-a" src="../../assets/RS3.jpg" alt="engine">
+                            <input type="number" class="form-control" placeholder="Quantity">
+                            <br>
                             <button class="btn btn-primary btn-cart" @click="addToCart(product)">ADD TO CART</button>
                         </div>
                     </div>
@@ -79,9 +91,11 @@
             <div class="col col-xxl-3 text-center my-col">
                 <transition name="fade" appear="">
                     <div class="card border-dark mb-3">
-                        <div class="card-header">{{ rs5Name }}</div>
+                        <div class="card-header">RS5</div>
                         <div class="card-body text-dark">
                             <img class="img-engine img-a" src="../../assets/RS5.jpg" alt="engine">
+                            <input type="number" class="form-control" placeholder="Quantity">
+                            <br>
                             <button class="btn btn-primary btn-cart" @click="addToCart(rs5Name)">ADD TO CART</button>
                         </div>
                     </div>
@@ -90,9 +104,11 @@
             <div class="col col-xxl-3 text-center my-col">
                 <transition name="fade" appear="">
                     <div class="card border-dark mb-3">
-                        <div class="card-header">{{ rs8.name }}</div>
+                        <div class="card-header">RS8</div>
                         <div class="card-body text-dark">
                             <img class="img-engine img-a" src="../../assets/RS8.jpg" alt="engine">
+                            <input type="number" class="form-control" placeholder="Quantity">
+                            <br>
                             <button class="btn btn-primary btn-cart" @click="addToCart(rs8)">ADD TO CART</button>
                         </div>
                     </div>
@@ -107,8 +123,16 @@
         name: 'Engine',
         data() {
             return {
-                rs8: { name: 'RS8', cost: 3 },
-                rs5Name: 'RS5'
+                engines: [
+                    { id: 1, name: 'RS7', price: 5000 },
+                    { id: 2, name: 'RS6-Avant', price: 4000 },
+                    { id: 3, name: 'RS6', price: 6000 },
+                    { id: 4, name: 'RS4-Avant', price: 7000 },
+                    { id: 5, name: 'RS4', price: 3000 },
+                    { id: 6, name: 'RS3', price: 8000 },
+                    { id: 7, name: 'RS5', price: 9000 },
+                    { id: 8, name: 'RS8', price: 2000 },
+                ],
             }
         },
         methods: {
@@ -121,5 +145,5 @@
 </script>
 
 <style scoped>
-
+/* v-for="engine in engines" :key="engine" :ShoppingCart="ShoppingCart" */
 </style>
--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -10,6 +10,9 @@
         <input type="text" :value="value" @input="updateValue">
         <p>{{ value }}</p>
         <br>
+        <p>{{ cart.RS8 }}</p>
+        <br>
+        <p>{{ cart.RS8 * 10000 }}</p>
         <br>
         <button class="btn btn-primary btn1" @click="increment(100)">Increment</button>
         <button class="btn btn-primary btn2" @click="decrement(100)">Decrement</button>
@@ -31,7 +34,10 @@
             ]),
             value() {
                 return this.$store.getters.value;
-            }
+            },
+            cartTotal() {
+                return (this.cart.RS8 * 10000).toFixed(2)
+            },
         },
         methods: {
             ...mapActions([
@@ -40,8 +46,26 @@
             ]),
             updateValue(event) {
                 this.$store.dispatch('updateValue', event.target.value);
+            },
+            addToCart(type) {
+                this.cart[type] += this.inventory[type]
             }
         },
+        data() {
+            return {
+                inventory: {
+                    RS3: 0,
+                    RS6: 0,
+                    RS8: 0,
+                },
+                cart: {
+                    RS3: 0,
+                    RS6: 0,
+                    RS8: 0,
+                }
+            } 
+        },
+        props: ['ShoppingCart'],
     }
 </script>
 
