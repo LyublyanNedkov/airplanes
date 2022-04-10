a81b902072fe175775e320588ab9cfee6ab22226
LyublyanNedkov <lyublyannedkov@gmail.com>
2022-04-10 21:55:17 +0300

    LN

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,8 +2,46 @@
     <div class="container container-fluid container-home">
         <div class="row justify-content-center">
             <div class="col col-sm-12 col-lg-12 col-xxl-12 text-center">
-                
+                <p class="audi_garage">AUDI GARAGE</p>
+                <img class="audi_pic" src="../assets/Bodykit.jpg" alt="pic">
             </div>
+            <div class="col col-sm-12 col-lg-12 col-xxl-12 text-center">
+                <p class="welcome_text">We pride ourselves on being the best shop for audi parts!
+                                    We can deliver the parts you need anywhere in the world 24/7.
+                                    We work only with original parts and you will be hard pressed 
+                                    to find lower prices then what we offer.
+                </p>
+                <img class="welcome_img" src="../assets/Suspension.jpg" alt="pic">
+            </div>
+            <div class="col col-sm-12 col-lg-12 col-xxl-12 text-center">
+                <p class="history_text">Our history:</p>
+                <p class="history_text_description">Our shop was opend in 1990 by Gustavo Lima.
+                                                    He wanted to create a direct conection between 
+                                                    the manufacturer and the customers. This way he 
+                                                    could skip the dealerships and deliver original
+                                                    parts with cheap prices to the customers. 
+                </p>
+                <img class="history_img" src="../assets/Audi.jpg" alt="pic">
+            </div>
+            <div class="row justify-content-center text-center my-row">
+                <div class="col col-xxl-12 text-center my-col">
+                    <p class="team_text_our">Our team:</p> 
+                </div>
+            </div>
+            <div class="row justify-content-center text-center my-row">
+                <div class="col col-xxl-4 text-center my-col">
+                    <p class="team_text">Tom Holland: Sales manager</p>
+                    <img class="Tom_Holland_pic" src="../assets/Tom_Holland.jpg" alt="pic">
+                </div>
+                <div class="col col-xxl-4 text-center my-col">
+                    <p class="team_text">Scarlet_Johansson: Human resources</p> 
+                    <img class="Scarlet_Johansson_pic" src="../assets/Scarlet_Johansson.jpg" alt="pic">
+                </div>
+                <div class="col col-xxl-4 text-center my-col">
+                    <p class="team_text">Robert_Downey_Jr: Technological R&D</p> 
+                    <img class="Robert_Downey_Jr" src="../assets/Robert_Downey_Jr.jpg" alt="pic">
+                </div>
+            </div>              
         </div>
     </div>
 </template>
--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -7,9 +7,6 @@
 
             <div class="product-display">
                 <div class="product-container">
-                    <div class="product-image">
-                        <img :src="image" alt="pic">
-                    </div>
                     <div class="product-info">
                         <h1>{{ title }}</h1>
                         <p v-if="inventory > 10">In stock</p>
@@ -138,7 +135,6 @@
                     RS8: 0,
                 },
                 cart: [],
-                image: '../assets/RS8.jpg',
                 premium: true,
                 product: 'Engines',
                 brand: 'AUDI',
