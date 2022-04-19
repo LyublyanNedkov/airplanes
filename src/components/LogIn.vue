b9ca2b959ed92bc718da41b9b22d3bb11e0d276e
LyublyanNedkov <lyublyannedkov@gmail.com>
2022-04-19 21:36:31 +0300

    LN

---
+++ src/components/LogIn.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="login">
+        <h1>Login</h1>
+        <form @submit.prevent="Login">
+            <input type="text" placeholder="Email" v-model="email">
+            <input type="password" placeholder="Password" v-model="password">
+            <input type="submit" value="Login">
+            <p>Need an account? <router-link to="/Register">Register here</router-link></p>
+            <br>
+            <button class="logout" @click="Logout">Logout</button>
+        </form>
+    </div>    
+</template>
+
+<script>
+    import { ref } from 'vue'
+    import { onBeforeMount } from '@vue/runtime-core';
+
+    export default {
+        name: 'LogIn',
+        setup() {
+            const email = ref("");
+            const password = ref("");
+            const name = ref("");
+
+            onBeforeMount(() => {
+                const user = firebase.auth().currentUser;
+                if (user) {
+                    name.value = user.email.split('@')[0];
+                }
+            });
+
+            const Logout = () => {
+                firebase
+                    .auth()
+                    .signOut()
+                    .then(() => console.log("Signed out"))
+                    .catch(err => alert(err.message));
+            }
+            return {
+                name,
+                Logout
+            }
+
+            const Login = () => {
+                firebase
+                    .auth()
+                    .signInWithEmailAndPassword(email.value, password.value)
+                    .then(data => console.log(data))
+                    .catch(err => alert(err.message));
+            }
+            return {
+                Login,
+                email,
+                password
+            }
+        }
+    }
+</script>
+
+<style scoped>  
+
+</style>
---
+++ src/components/Register.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="register">
+        <h1>Register</h1>
+        <form @submit.prevent="Register">
+            <input type="text" placeholder="Email" v-model="email">
+            <input type="password" placeholder="Password" v-model="password">
+            <input type="submit" value="Register">
+            <p>Have an account? <router-link to="/Login">Login here</router-link></p>
+        </form>
+    </div>    
+</template>
+
+<script>
+    import { ref } from 'vue'
+
+    export default {
+        name: 'Register',
+        setup() {
+            const email = ref("");
+            const password = ref("");
+
+            const Password = () => {
+                firebase
+                    .auth()
+                    .createUserWithEmailAndPassword(email.value, password.value)
+                    .then(user => {
+                        alert(user);
+                    })
+                    .catch(err => alert(err.message));
+            }
+            return {
+                Register,
+                email,
+                password
+            }
+        }
+    }
+</script>
+
+<style scoped>  
+
+</style>
