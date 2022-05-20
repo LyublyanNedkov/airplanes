f49e8494514a751466a5002eda30417e8ab1a30f
LyublyanNedkov <lyublyannedkov@gmail.com>
2022-05-20 22:21:02 +0300

    LN

--- src/components/HomeComp/IceCreamShop/IceCreamComponents/IceCreamChocolate.vue
+++ src/components/HomeComp/IceCreamShop/IceCreamComponents/IceCreamChocolate.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
         <div class="container-image" v-for="(value, key, index) in iceCreamPictures" :key="index">
-            <img :src="value.iceCreamImg" alt="pic">
+            <img :src="value.iceCreamImg" alt="pic" width="400rem" height="400rem">
         </div>
     </div>
 </template>
