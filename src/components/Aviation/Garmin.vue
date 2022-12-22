4982d75d601e7f7d1a6784839740a8068dcd2fc5
LyublyanNedkov <lyublyannedkov@gmail.com>
2022-12-22 15:38:07 +0200

    LN

---
+++ src/components/Aviation/Garmin.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="container container-engine" id="audi-engine">
+        <div class="row justify-content-center text-center">
+            <div class="col col-xxl-3 text-center my-col" v-for="(value, key, index) in koli" :key="index">
+                <transition name="fade" appear="">
+                    <div class="card border-dark mb-3">
+                        <div class="card-header card-color" :class="value.color">{{ value.name }}</div>
+                        <div class="card-body text-dark">
+                            <img class="img-engine img-a" :src="value.pictures" alt="pic">
+                        </div>
+                    </div>
+                </transition>
+            </div>
+        </div>
+        <img class="AirportImg" src="../../assets/Airport.jpg" alt="pic">
+        <img :src="value.pictures" alt="pic">
+        <p>RWJIWRIJGPIRJGIR</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Garmin',
+        props: ['models'],
+        data() {
+            return {
+                koli: [
+                    { name: 'RS7', price: 10000, inStock: 11, color: 'bg-primary', pictures: require('@/assets/RS7.jpg') },
+                    { name: 'RS6-Avant', price: 11000, inStock: 12, color: 'bg-primary', pictures: require('@/assets/RS6-Avant.jpg') },
+                    { name: 'RS6', price: 12000, inStock: 13, color: 'bg-primary', pictures: require('@/assets/RS6.jpg') },
+                    { name: 'RS4-Avant', price: 13000, inStock: 14, color: 'bg-primary', pictures: require('@/assets/RS4-Avant.jpg') },
+                    { name: 'RS4', price: 14000, inStock: 15, color: 'bg-primary', pictures: require('@/assets/RS4.jpg') },
+                    { name: 'RS3', price: 15000, inStock: 16, color: 'bg-primary', pictures: require('@/assets/RS3.jpg') },
+                    { name: 'RS5', price: 16000, inStock: 17, color: 'bg-primary', pictures: require('@/assets/RS5.jpg') },
+                    { name: 'RS8', price: 17000, inStock: 18, color: 'bg-primary', pictures: require('@/assets/RS8.jpg') }
+                ]
+            }
+        },
+        methods: {
+            submit() {
+                this.$http.post('', this.name)
+                    .then(response => {
+                        console.log(response);
+                    }, error => {
+                        console.log(error);
+                    });
+            },
+
+            fetchData() {
+                this.$http.get('')
+                    .then(response => {
+                        return response.json();
+                    })
+                    .then(data => console.log(data));
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Aviation/Safran.vue
@@ -0,0 +1,174 @@
+<template>
+    <div class="container container-engine" id="audi-engine">
+        <div class="row justify-content-center text-center">
+            <div class="col col-xxl-3 text-center my-col">
+                <transition name="fade" appear="">
+                    <div class="card border-dark mb-3">
+                        <div class="card-header card-color bg-danger">{{ list.audi_rims }}</div>
+                        <div class="card-body text-dark">
+                            <img class="img-engine img-a" src="../../assets/W1.jpg" alt="engine">
+                            <p>Price: {{ price.RS7 }}$</p>
+                            <p>In stock: {{ inStock.RS7 }} units</p>
+                            <input type="number" class="form-control" placeholder="Quantity">
+                            <br>
+                            <button class="btn btn-primary btn-cart" @click="addToCart()">ADD TO CART</button>
+                        </div>
+                    </div>
+                </transition>
+            </div>
+            <div class="col col-xxl-3 text-center my-col">
+                <transition name="fade" appear="">
+                    <div class="card border-dark mb-3">
+                        <div class="card-header bg-danger">{{ list.audi_rims }}</div>
+                        <div class="card-body text-dark">
+                            <img class="img-engine img-a" src="../../assets/W2.jpg" alt="engine">
+                            <p>Price: {{ price.RS6_Avant }}$</p>
+                            <p>In stock: {{ inStock.RS6_Avant }} units</p>
+                            <input type="number" class="form-control" placeholder="Quantity">
+                            <br>
+                            <button class="btn btn-primary btn-cart" @click="addToCart()">ADD TO CART</button>
+                        </div>
+                    </div>
+                </transition>
+            </div>
+            <div class="col col-xxl-3 text-center my-col">
+                <transition name="fade" appear="">
+                    <div class="card border-dark mb-3">
+                        <div class="card-header bg-danger">{{ list.audi_rims }}</div>
+                        <div class="card-body text-dark">
+                            <img class="img-engine img-a" src="../../assets/W3.jpg" alt="engine">
+                            <p>Price: {{ price.RS6 }}$</p>
+                            <p>In stock: {{ inStock.RS6 }} units</p>
+                            <input type="number" class="form-control" placeholder="Quantity">
+                            <br>
+                            <button class="btn btn-primary btn-cart" @click="addToCart()">ADD TO CART</button>
+                        </div>
+                    </div>
+                </transition>
+            </div>
+            <div class="col col-xxl-3 text-center my-col">
+                <transition name="fade" appear="">
+                    <div class="card border-dark mb-3">
+                        <div class="card-header bg-danger">{{ list.audi_rims }}</div>
+                        <div class="card-body text-dark">
+                            <img class="img-engine img-a" src="../../assets/W4.jpg" alt="engine">
+                            <p>Price: {{ price.RS4_Avant }}$</p>
+                            <p>In stock: {{ inStock.RS4_Avant }} units</p>
+                            <input type="number" class="form-control" placeholder="Quantity">
+                            <br>
+                            <button class="btn btn-primary btn-cart" @click="addToCart()">ADD TO CART</button>
+                        </div>
+                    </div>
+                </transition>
+            </div>
+        </div>   
+        <div class="row justify-content-center text-center">
+            <div class="col col-xxl-3 text-center my-col">
+                <transition name="fade" appear="">
+                    <div class="card border-dark mb-3">
+                        <div class="card-header bg-danger">{{ list.audi_rims }}</div>
+                        <div class="card-body text-dark">
+                            <img class="img-engine img-a" src="../../assets/W5.webp" alt="engine">
+                            <p>Price: {{ price.RS4 }}$</p>
+                            <p>In stock: {{ inStock.RS4 }} units</p>
+                            <input type="number" class="form-control" placeholder="Quantity">
+                            <br>
+                            <button class="btn btn-primary btn-cart" @click="addToCart()">ADD TO CART</button>
+                        </div>
+                    </div>
+                </transition>
+            </div>
+            <div class="col col-xxl-3 text-center my-col">
+                <transition name="fade" appear="">
+                    <div class="card border-dark mb-3">
+                        <div class="card-header bg-danger">{{ list.audi_rims }}</div>
+                        <div class="card-body text-dark">
+                            <img class="img-engine img-a" src="../../assets/W6.jpg" alt="engine">
+                            <p>Price: {{ price.RS3 }}$</p>
+                            <p>In stock: {{ inStock.RS3 }} units</p>
+                            <input type="number" class="form-control" placeholder="Quantity">
+                            <br>
+                            <button class="btn btn-primary btn-cart" @click="addToCart()">ADD TO CART</button>
+                        </div>
+                    </div>
+                </transition>
+            </div>
+            <div class="col col-xxl-3 text-center my-col">
+                <transition name="fade" appear="">
+                    <div class="card border-dark mb-3">
+                        <div class="card-header bg-danger">{{ list.audi_rims }}</div>
+                        <div class="card-body text-dark">
+                            <img class="img-engine img-a" src="../../assets/W7.jpg" alt="engine">
+                            <p>Price: {{ price.RS5 }}$</p>
+                            <p>In stock: {{ inStock.RS5 }} units</p>
+                            <input type="number" class="form-control" placeholder="Quantity">
+                            <br>
+                            <button class="btn btn-primary btn-cart" @click="addToCart()">ADD TO CART</button>
+                        </div>
+                    </div>
+                </transition>
+            </div>
+            <div class="col col-xxl-3 text-center my-col">
+                <transition name="fade" appear="">
+                    <div class="card border-dark mb-3">
+                        <div class="card-header bg-danger">{{ list.audi_rims }}</div>
+                        <div class="card-body text-dark">
+                            <img class="img-engine img-a" src="../../assets/W8.jpg" alt="engine">
+                            <p>Price: {{ price.RS8 }}$</p>
+                            <p>In stock: {{ inStock.RS8 }} units</p>
+                            <input type="number" class="form-control" placeholder="Quantity">
+                            <br>
+                            <button class="btn btn-primary btn-cart" @click="addToMyCart()">ADD TO CART</button>
+                        </div>
+                    </div>
+                </transition>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Safran',
+    data() {
+            return {
+                list: {
+                    audi_rims: 'Audi rims'
+                },
+                price: {
+                    RS7: 10000,
+                    RS6_Avant: 11000,
+                    RS6: 12000,
+                    RS4_Avant: 13000,
+                    RS4: 14000,
+                    RS3: 15000,
+                    RS5: 16000,
+                    RS8: 17000
+                },
+                inStock: {
+                    RS6_Avant: 11,
+                    RS6: 12,
+                    RS4_Avant: 13,
+                    RS4: 14,
+                    RS7: 10,
+                    RS3: 15,
+                    RS5: 16,
+                    RS8: 17
+                }
+            }
+        },
+        props: [],
+        methods: {
+            addToCart() {
+                this.$emit('add-to-cart')
+            }
+        },   
+        created() {
+            bus.$on('dynheader')
+        },
+}
+</script>
+
+<style scoped>
+
+</style>
