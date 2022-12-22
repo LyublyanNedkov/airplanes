f9d12fbb46e806d3cf3b7aaffde0f93e9693601e
LyublyanNedkov <lyublyannedkov@gmail.com>
2022-12-22 15:47:25 +0200

    LN

--- src/components/Audi.vue
+++ src/components/Audi.vue
@@ -3,80 +3,41 @@
         <div class="row justify-content-center text-center my-row1">
             <div class="col col-xxl-3 text-center my-col">
                 <transition name="fade" appear="">
-                    <div class="card text-white bg-primary mb-3">
+                    <div class="card text-white bg-black mb-3">
                         <div class="card-header">
-                            <router-link class="btn-engine btn-a" to="/Audi/Engine">Engine</router-link>
+                            <router-link class="btn-a" to="/Aviation/Garmin">Garmin</router-link>
                         </div>
                     </div>
                 </transition> 
             </div>
             <div class="col col-xxl-3 text-center my-col">
                 <transition name="fade" appear="">
-                    <div class="card text-white bg-secondary mb-3">
+                    <div class="card text-white bg-black mb-3">
                         <div class="card-header">
-                            <router-link class="btn-gearbox btn-a" to="/Audi/Gearbox">Gearbox</router-link>
+                            <router-link class="btn-a" to="/Aviation/PrattWhitney">Pratt & Whitney</router-link>
                         </div>
                     </div>
                 </transition>
             </div>
             <div class="col col-xxl-3 text-center my-col">
                 <transition name="fade" appear="">
-                    <div class="card text-white bg-success mb-3">
+                    <div class="card text-white bg-black mb-3">
                         <div class="card-header">
-                            <router-link class="btn-bodykit btn-a" to="/Audi/Bodykit">Bodykit</router-link>
+                            <router-link class="btn-a" to="/Aviation/Honeywell">Honeywell</router-link>
                         </div>
                     </div>
                 </transition>
             </div>
             <div class="col col-xxl-3 text-center my-col">
                 <transition name="fade" appear="">
-                    <div class="card text-white bg-danger mb-3">
+                    <div class="card text-white bg-black mb-3">
                         <div class="card-header">
-                            <router-link class="btn-wheels btn-a" to="/Audi/Wheels">Wheels</router-link>
+                            <router-link class="btn-a" to="/Aviation/Safran">Safran</router-link>
                         </div>
                     </div>
                 </transition>
-            </div>
-        <div class="row justify-content-center text-center my-row2">
-            <div class="col col-xxl-3 text-center my-col">
-                <transition name="fade" appear="">
-                    <div class="card text-white bg-warning mb-3">
-                        <div class="card-header">
-                            <router-link class="btn-breaks btn-a" to="/Audi/Breaks">Breaks</router-link>
-                        </div>
-                    </div>
-                </transition>
-            </div>
-            <div class="col col-xxl-3 text-center my-col">
-                <transition name="fade" appear="">
-                    <div class="card text-white bg-info mb-3">
-                        <div class="card-header">
-                            <router-link class="btn-suspension btn-a" to="/Audi/Suspension">Suspension</router-link>
-                        </div>
-                    </div>
-                </transition>
-            </div>
-            <div class="col col-xxl-3 text-center my-col">
-                <transition name="fade" appear="">
-                    <div class="card bg-my mb-3">
-                        <div class="card-header">
-                            <router-link class="btn-turbocharger btn-a" to="/Audi/Turbocharger">Turbocharger</router-link>
-                        </div>
-                    </div>
-                </transition>
-            </div>
-            <div class="col col-xxl-3 text-center my-col">
-                <transition name="fade" appear="">
-                    <div class="card text-white bg-dark mb-3">
-                        <div class="card-header">
-                            <router-link class="btn-exhaust btn-a" to="/Audi/Exhaust_system">Exhaust system</router-link>
-                        </div>
-                    </div>
-                </transition>
-            </div>
-        </div>           
+            </div>          
         </div>
-        <router-view />
     </div>
 </template>
 
